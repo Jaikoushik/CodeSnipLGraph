3037704b22392db4513e0d474d35b042acf9529e
Koushik Reddy <koushikreddyab@gmail.com>
2026-03-12 13:26:45 +0530

    Change in chatbot

--- langgraph_chatbot/streamlit_frontend.py
+++ langgraph_chatbot/streamlit_frontend.py
@@ -1,50 +1,34 @@
 import streamlit as st
+from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
-from langgraph_backend import chatbot   # import compiled graph from backend
 
-st.set_page_config(page_title="LangGraph Chatbot", page_icon="🤖")
+# st.session_state -> dict -> 
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
-st.title("🤖 LangGraph Chatbot (Groq + Streamlit)")
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 
-# Thread ID for LangGraph memory
-thread_id = "streamlit-thread"
+#{'role': 'user', 'content': 'Hi'}
+#{'role': 'assistant', 'content': 'Hi=ello'}
 
-config = {"configurable": {"thread_id": thread_id}}
-
-# Display previous chat messages
-for msg in st.session_state.messages:
-    with st.chat_message(msg["role"]):
-        st.markdown(msg["content"])
-
-# Chat input box
-user_input = st.chat_input("Type your message...")
+user_input = st.chat_input('Type here')
 
 if user_input:
 
-    # Show user message
-    with st.chat_message("user"):
-        st.markdown(user_input)
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
 
-    st.session_state.messages.append(
-        {"role": "user", "content": user_input}
-    )
-
-    # Call LangGraph chatbot
-    response = chatbot.invoke(
-        {"messages": [HumanMessage(content=user_input)]},
-        config=config
-    )
-
-    bot_reply = response["messages"][-1].content
-
-    # Show assistant response
-    with st.chat_message("assistant"):
-        st.markdown(bot_reply)
-
-    st.session_state.messages.append(
-        {"role": "assistant", "content": bot_reply}
-    )
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    
+    ai_message = response['messages'][-1].content
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
